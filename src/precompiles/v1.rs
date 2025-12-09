7c3eb606ec98a4cf5f88d29ebe8c1789ef4ff7d2
Vlad Bochok <vladbochok1@gmail.com>
2025-12-09 15:15:08 +0100

    fmt

--- src/precompiles/v1.rs
+++ src/precompiles/v1.rs
@@ -1,11 +1,11 @@
 //! Precompiles V1
 //!
 //! Initial precompiles implementation kept only for legacy compatibility.
-//! This version is not used by the current system. 
-//! 
+//! This version is not used by the current system.
+//!
 //! Please note it does not charge gas correctly, and is *NOT*
 //! fully aligned with the ZKsync OS implementation.
-//! As a result, the consistency checker may report divergences. 
+//! As a result, the consistency checker may report divergences.
 //! However there were no divergance trigger on Stage/Testnet/Mainnet chains.
 pub mod deployer;
 pub mod l1_messenger;
